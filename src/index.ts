6d61a2938309c7fc9ab109faee07f50e49727c3f
Victor Gabriel Vieira Francisco <victorgabrielfrancisco@gmail.com>
2024-02-17 02:32:42 -0300

    feat: install jwt

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import express from "express";
 import rootRouter from "./routes";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +13,6 @@ app.use(express.text({ type: "text/html" }));
 
 app.use("/api", rootRouter);
 
-app.listen(Number(port), "0.0.0.0", () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log("app iniciado");
 });
